7e60f51203ca994270fd0c74aaa41a85d08a69f4
“vi3003” <vidhijain3003@gmail.com>
2023-01-11 22:41:10 +0530

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your code here
+
+let count;
+document.getElementById("evaluatedText").value = ""; // initially string is empty so count is also empty
+
+document.addEventListener("keyup", function () {
+  count = document.getElementById("evaluatedText").value; //here this value will return string
+  document.querySelector("#letterCount").textContent = count.length;
+  //   console.log(count);
+});
